94bbefcc337a776cfbcbe5c34c2857b1ea1b6c43
Nuxt.Macias <edwardavid26@hotmail.com>
2023-10-08 15:37:52 -0500

    resumen diferencias option API VS composition API

--- src/components/template-ref.vue
+++ src/components/template-ref.vue
@@ -4,6 +4,13 @@
     <div>Username:  {{ username }}</div>
     <button ref="btn">click!</button>
     </div>
+    <p>Se explicaron las API de composition y de options, donde se enfoca en ver las diferencias
+        pero no se entregan como tal una diferencia para el lado negativo, son maneras de trabajar,
+        Por ejemplo cuando se quiere trabajar con muchas funciones pues el composition API es mucho mas
+        facil de trabajar, pero el option API es mas para funciones basicas, de igual manera el acceso 
+        a los datos para que se mantengan reactivos es importante, así que mejor verificar
+        eso siempre.
+    </p>
 </template>
 
 <script>
